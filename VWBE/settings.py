75badfed8686f277af30468b685c89321bc5fb73
Valour Wealth <valourwealth1@gmail.com>
2025-06-28 16:20:52 +0500

    a

--- VWBE/settings.py
+++ VWBE/settings.py
@@ -36,7 +36,9 @@ ALLOWED_HOSTS = [
     "https://backend-production-1e63.up.railway.app",
     "https://valourwealth-frontend-psi.vercel.app",
     "https://tradegptlatest.vercel.app",
+    "tradegptbackend-production.up.railway.app",
 ]
+
 
 # This is for cloudflare video storage configurations:
 
@@ -164,6 +166,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://backend-production-1e63.up.railway.app",
     "https://valourwealth-frontend-psi.vercel.app",
     "https://tradegptlatest.vercel.app",
+    "tradegptbackend-production.up.railway.app",
 ]
 
 
