d75cf12478e4ec728bdbdc388eaf54156e79403c
Valour Wealth <valourwealth1@gmail.com>
2025-07-17 14:14:34 +0500

    agaga

--- main/consumers.py
+++ main/consumers.py
@@ -1,7 +1,8 @@
+
 # import json
 # from channels.generic.websocket import AsyncWebsocketConsumer
 # from channels.db import database_sync_to_async
-# from .models import ChatThread, Message
+# from .models import ChatThread, Ana_Message
 # from django.contrib.auth import get_user_model
 
 # User = get_user_model()
@@ -9,6 +10,11 @@
 # class ChatConsumer(AsyncWebsocketConsumer):
 #     async def connect(self):
 #         self.user = self.scope["user"]
+
+#         if not self.user.is_authenticated:
+#             await self.close()
+#             return
+
 #         self.other_user_id = self.scope["url_route"]["kwargs"]["user_id"]
 #         self.room_name = f"chat_{min(self.user.id, int(self.other_user_id))}_{max(self.user.id, int(self.other_user_id))}"
 #         self.room_group_name = f"chat_{self.room_name}"
@@ -18,24 +24,31 @@
 
 #     async def disconnect(self, close_code):
 #         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+#         print(f"[WebSocket Disconnected] code={close_code} user={self.user.username}")
 
 #     async def receive(self, text_data):
-#         data = json.loads(text_data)
-#         message = data["message"]
-#         sender = self.user
-#         receiver = await self.get_user(int(self.other_user_id))
+#         try:
+#             data = json.loads(text_data)
+#             message = data.get("message")
+#             if not message:
+#                 return
 
-#         thread = await self.get_or_create_thread(sender, receiver)
-#         msg = await self.create_message(thread, sender, message)
+#             sender = self.user
+#             receiver = await self.get_user(int(self.other_user_id))
 
-#         await self.channel_layer.group_send(
-#             self.room_group_name,
-#             {
-#                 "type": "chat_message",
-#                 "message": message,
-#                 "sender": sender.username,
-#             }
-#         )
+#             thread = await self.get_or_create_thread(sender, receiver)
+#             msg = await self.create_message(thread, sender, message)
+
+#             await self.channel_layer.group_send(
+#                 self.room_group_name,
+#                 {
+#                     "type": "chat_message",
+#                     "message": message,
+#                     "sender": sender.username,
+#                 }
+#             )
+#         except Exception as e:
+#             print("WebSocket error:", e)
 
 #     async def chat_message(self, event):
 #         await self.send(text_data=json.dumps({
@@ -49,16 +62,18 @@
 
 #     @database_sync_to_async
 #     def get_or_create_thread(self, user1, user2):
-#         return ChatThread.objects.get_or_create(user=user1, analyst=user2)[0]
+#         if user1.profile.role == 'analyst':
+#             return ChatThread.objects.get_or_create(user=user2, analyst=user1)[0]
+#         else:
+#             return ChatThread.objects.get_or_create(user=user1, analyst=user2)[0]
 
 #     @database_sync_to_async
 #     def create_message(self, thread, sender, content):
-#         return Message.objects.create(thread=thread, sender=sender, content=content)
+#         return Ana_Message.objects.create(thread=thread, sender=sender, content=content)
 
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from .models import ChatThread, Ana_Message
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -93,7 +108,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             receiver = await self.get_user(int(self.other_user_id))
 
             thread = await self.get_or_create_thread(sender, receiver)
-            msg = await self.create_message(thread, sender, message)
+            await self.create_message(thread, sender, message)
 
             await self.channel_layer.group_send(
                 self.room_group_name,
@@ -118,6 +133,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @database_sync_to_async
     def get_or_create_thread(self, user1, user2):
+        from .models import ChatThread  # ✅ moved inside method
+
         if user1.profile.role == 'analyst':
             return ChatThread.objects.get_or_create(user=user2, analyst=user1)[0]
         else:
@@ -125,4 +142,5 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @database_sync_to_async
     def create_message(self, thread, sender, content):
+        from .models import Ana_Message  # ✅ moved inside method
         return Ana_Message.objects.create(thread=thread, sender=sender, content=content)
